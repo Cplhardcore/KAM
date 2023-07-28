48d4f628237cc777ae9262cd03e7768f7bb1d7f9
Cplhardcore <135324281+Cplhardcore@users.noreply.github.com>
2023-07-27 22:55:32 -0700

    Addition of syringes to KAM

--- addons/pharma/CfgWeapons.hpp
+++ addons/pharma/CfgWeapons.hpp
@@ -40,6 +40,14 @@ class CfgWeapons {
             mass = 0.1;
         }; 
     };
+    class kat_syringeBase: ACE_ItemCore {
+        scope = 1;
+        ace_arsenal_hide = 1;
+        picture = QPATHTOF(ui\syringe_item.paa);
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 0.1;
+        };
+    };
     class kat_IV_16: ACE_ItemCore {
         scope = 2;
         displayName = CSTRING(IV_16_Display);
@@ -83,6 +91,10 @@ class CfgWeapons {
             mass = 0.2;
         };
     };
+    class kat_EACASyringe: kat_syringeBase {
+        displayName = CSTRING(EACA_Display);
+        descriptionShort = CSTRING(EACA_DescShort);
+    };
     class kat_TXA: ACE_ItemCore {
         scope = 2;
         displayName = CSTRING(TXA_Box_Display);
@@ -91,7 +103,11 @@ class CfgWeapons {
         descriptionShort = CSTRING(TXA_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.5;
-        }; 
+        };
+    };
+    class kat_TXASyringe: kat_syringeBase {
+        displayName = CSTRING(TXA_Box_Display);
+        descriptionShort = CSTRING(TXA_DescShort);
     };
     class kat_norepinephrine: ACE_ItemCore {
         scope = 2;
@@ -101,7 +117,11 @@ class CfgWeapons {
         descriptionShort = CSTRING(Norep_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.5;
-        }; 
+        };
+    };
+    class kat_norepinephrineSyringe: kat_syringeBase {
+        displayName = CSTRING(Norep_Box_Display);
+        descriptionShort = CSTRING(Norep_DescShort);
     };
     class kat_phenylephrine: ACE_ItemCore {
         scope = 2;
@@ -111,7 +131,11 @@ class CfgWeapons {
         descriptionShort = CSTRING(Phenyl_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.5;
-        }; 
+        };
+    };
+    class kat_phenylephrineSyringe: kat_syringeBase {
+        displayName = CSTRING(Phenyl_Box_Display);
+        descriptionShort = CSTRING(Phenyl_DescShort);
     };
     class kat_nitroglycerin: ACE_ItemCore {
         scope = 2;
@@ -121,7 +145,11 @@ class CfgWeapons {
         descriptionShort = CSTRING(Nitro_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.5;
-        }; 
+        };
+    };
+    class kat_nitroglycerinSyringe: kat_syringeBase {
+        displayName = CSTRING(Nitro_Box_Display);
+        descriptionShort = CSTRING(Nitro_DescShort);
     };
     class kat_amiodarone: ACE_ItemCore {
         scope = 2;
@@ -131,7 +159,11 @@ class CfgWeapons {
         descriptionShort = CSTRING(Amiodarone_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.5;
-        }; 
+        };
+    };
+    class kat_amiodaroneSyringe: kat_syringeBase {
+        displayName = CSTRING(Amiodarone_Box_Display);
+        descriptionShort = CSTRING(Amiodarone_DescShort);
     };
     class kat_lidocaine: ACE_ItemCore {
         scope = 2;
@@ -141,7 +173,11 @@ class CfgWeapons {
         descriptionShort = CSTRING(Lidocaine_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.5;
-        }; 
+        };
+    };
+    class kat_lidocaineSyringe: kat_syringeBase {
+        displayName = CSTRING(Lidocaine_Box_Display);
+        descriptionShort = CSTRING(Lidocaine_DescShort);
     };
     class kat_atropine: ACE_ItemCore {
         scope = 2;
@@ -151,7 +187,11 @@ class CfgWeapons {
         descriptionShort = CSTRING(Atropine_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.5;
-        }; 
+        };
+    };
+    class kat_atropineSyringe: kat_syringeBase {
+        displayName = CSTRING(Atropine_Box_Display);
+        descriptionShort = CSTRING(Atropine_DescShort); 
     };
     class kat_ketamine: ACE_ItemCore {
         scope = 2;
@@ -161,7 +201,11 @@ class CfgWeapons {
         descriptionShort = CSTRING(Ketamine_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.5;
-        }; 
+        };
+    };
+    class kat_ketamineSyringe: kat_syringeBase {
+        displayName = CSTRING(Ketamine_Box_Display);
+        descriptionShort = CSTRING(Ketamine_DescShort);
     };
     class kat_fentanyl: ACE_ItemCore {
         scope = 2;
@@ -171,7 +215,11 @@ class CfgWeapons {
         descriptionShort = CSTRING(Fentanyl_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.5;
-        }; 
+        };
+    };
+    class kat_fentanylSyringe: kat_syringeBase {
+        displayName = CSTRING(Fentanyl_Box_Display);
+        descriptionShort = CSTRING(Fentanyl_DescShort);
     };
     class kat_nalbuphine: ACE_ItemCore {
         scope = 2;
@@ -181,7 +229,11 @@ class CfgWeapons {
         descriptionShort = CSTRING(Nalbuphine_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.5;
-        }; 
+        };
+    };
+    class kat_nalbuphineSyringe: kat_syringeBase {
+        displayName = CSTRING(Nalbuphine_Box_Display);
+        descriptionShort = CSTRING(Nalbuphine_DescShort);
     };
         class kat_lorazepam: ACE_ItemCore {
         scope = 2;
@@ -191,7 +243,11 @@ class CfgWeapons {
         descriptionShort = CSTRING(Lorazepam_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.2;
-        }; 
+        };
+    };
+    class kat_lorazepamSyringe: kat_syringeBase {
+        displayName = CSTRING(Lorazepam_displayName);
+        descriptionShort = CSTRING(Lorazepam_DescShort);
     };
     class kat_flumazenil: ACE_ItemCore {
         scope = 2;
@@ -201,7 +257,11 @@ class CfgWeapons {
         descriptionShort = CSTRING(Flumazenil_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.2;
-        }; 
+        };
+    };
+    class kat_flumazenilSyringe: kat_syringeBase {
+        displayName = CSTRING(Flumazenil_displayName);
+        descriptionShort = CSTRING(Flumazenil_DescShort);
     };
     class kat_etomidate: ACE_ItemCore {
         scope = 2;
@@ -211,6 +271,19 @@ class CfgWeapons {
         descriptionShort = CSTRING(Etomidate_DescShort);
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.2;
-        }; 
+        };
+    };
+    class kat_etomidateSyringe: kat_syringeBase {
+        displayName = CSTRING(Etomidate_displayName);
+        descriptionShort = CSTRING(Etomidate_DescShort);
+    };
+    class kat_syringe: ACE_ItemCore {
+        scope = 2;
+        displayName = CSTRING(Syringe_displayName);
+        descriptionShort = CSTRING(Syringe_DescUse);
+        picture = QPATHTOF(ui\syringe_item.paa);
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 0.1;
+        };
     };
 };
