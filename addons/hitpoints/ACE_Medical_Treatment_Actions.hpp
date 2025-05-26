9c6575c6835bc7cd4334bc4543868d23006a6b19
Cplhardcore <135324281+Cplhardcore@users.noreply.github.com>
2025-05-25 22:20:36 -0700

    guh

--- addons/hitpoints/ACE_Medical_Treatment_Actions.hpp
+++ addons/hitpoints/ACE_Medical_Treatment_Actions.hpp
@@ -1,6 +1,149 @@
 class ACE_Medical_Treatment_Actions {
     class CheckPulse;
     class BasicBandage;
+    class CPR;
+    class Hemostat: BasicBandage {
+        displayName = CSTRING(Hemostat);
+        displayNameProgress = CSTRING(Hemostat_Progress);
+        icon = QPATHTOF(ui\Hemostat.paa);
+        items[] = {"kat_Hemostatic_Injector"};
+        allowSelfTreatment = 1;
+        allowedSelections[] = {"LeftArm", "RightArm", "LeftLeg", "RightLeg", "UpperLeftArm", "UpperRightArm", "UpperLeftLeg", "UpperRightLeg"};
+        treatmentTime = 6;
+    };
+    class fourByfour_Gauze: BasicBandage {
+        displayName = CSTRING(4X4_Gauze);
+        displayNameProgress = CSTRING(4X4_Gauze_Progress);
+        icon = QPATHTOF(ui\4X4_Gauze.paa);
+        items[] = {"kat_4X4_Gauze"};
+    };
+    class Compressed_Gauze: BasicBandage {
+        displayName = CSTRING(Compressed_Gauze);
+        displayNameProgress = CSTRING(Compressed_Gauze_Progress);
+        icon = QPATHTOF(ui\Compressed_Gauze.paa);
+        allowedSelections[] = {"Body", "LeftArm", "RightArm", "LeftLeg", "RightLeg", "UpperLeftArm", "UpperRightArm", "UpperLeftLeg", "UpperRightLeg"};
+        items[] = {"kat_Compressed_Gauze"};
+    };
+    class Hemostatic_Gauze: BasicBandage {
+        displayName = CSTRING(Hemostatic_Gauze);
+        displayNameProgress = CSTRING(Hemostatic_Gauze_Progress);
+        icon = QPATHTOF(ui\Hemostatic_Gauze.paa);
+        items[] = {"kat_Hemostatic_Gauze"};
+    };
+    class Adhesive_Bandage: BasicBandage {
+        displayName = CSTRING(Adhesive_Bandage);
+        displayNameProgress = CSTRING(Adhesive_Bandage_Progress);
+        icon = QPATHTOF(ui\Adhesive_Bandage.paa);
+        items[] = {"kat_Adhesive_Bandage"};
+        treatmentTime = 3;
+    };
+    class Burn_Dressing: BasicBandage {
+        displayName = CSTRING(Burn_Dressing);
+        displayNameProgress = CSTRING(Burn_Dressing_Progress);
+        icon = QPATHTOF(ui\Burn_Dressing.paa);
+        condition = QFUNC(canBurnBandage);
+        items[] = {"kat_Burn_Dressing"};
+    };
+    class Abdominal_Pad: BasicBandage {
+        displayName = CSTRING(Abdominal_Pad);
+        displayNameProgress = CSTRING(Abdominal_Pad_Progress);
+        icon = QPATHTOF(ui\Abdominal_Pad.paa);
+        items[] = {"kat_Abdominal_Pad"};
+        allowedSelections[] = {"Body"};
+        callbackSuccess = QFUNC(ABDPad);
+        treatmentTime = 6;
+    };
+    class ETD: BasicBandage {
+        displayName = CSTRING(ETD);
+        displayNameProgress = CSTRING(ETD_Progress);
+        icon = QPATHTOF(ui\ETD.paa);
+        callbackSuccess = QACEFUNC(medical_treatment,bandage);
+        items[] = {"kat_ETD"};
+    };
+    class Elastic_Wrap: BasicBandage {
+        displayName = CSTRING(Elastic_Wrap);
+        displayNameProgress = CSTRING(Elastic_Wrap_Progress);
+        icon = QPATHTOF(ui\Elastic_Wrap.paa);
+        condition = QFUNC(canWrapWound);
+        treatmentTime = QFUNC(getWrapTime);
+        callbackSuccess = QFUNC(wrapWound);
+        items[] = {"kat_Elastic_Wrap"};
+    };
+    class Elastic_Wrap_Splint: BasicBandage {
+        displayName = CSTRING(Elastic_Wrap_Splint);
+        displayNameProgress = CSTRING(Elastic_Wrap_Splint_Progress);
+        icon = QPATHTOF(ui\Elastic_Wrap.paa);
+        callbackSuccess = QFUNC(wrapSplint);
+        condition = QFUNC(canWrapSplint);
+        treatmentTime = 12;
+        items[] = {"kat_Elastic_Wrap"};
+    };
+    class Roller_Gauze: BasicBandage {
+        displayName = CSTRING(Roller_Gauze);
+        displayNameProgress = CSTRING(Roller_Gauze_Progress);
+        icon = QPATHTOF(ui\Roller_Gauze.paa);
+        condition = QFUNC(canWrapWound);
+        treatmentTime = QFUNC(getWrapTime);
+        callbackSuccess = QFUNC(wrapWound);
+        items[] = {"kat_Compressed_Gauze"};
+    };
+    class Ice_Pack: BasicBandage {
+        displayName = CSTRING(Ice_Pack);
+        displayNameProgress = CSTRING(Ice_Pack_Progress);
+        icon = QPATHTOF(ui\Ice_Pack.paa);
+        condition = QFUNC(canIceJoint);
+        category = "surgery";
+        medicRequired = 0;
+        treatmentTime = QGVAR(iceTime);
+        callbackSuccess = QFUNC(IceJoint);
+        items[] = {"kat_Ice_Pack"};
+    };
+    class Elastic_Wrap_Joint: BasicBandage {
+        displayName = CSTRING(Elastic_Wrap_Joint);
+        displayNameProgress = CSTRING(Elastic_Wrap_Joint_Progress);
+        icon = QPATHTOF(ui\Elastic_Wrap.paa);
+        category = "surgery";
+        callbackSuccess = QFUNC(wrapJoint);
+        condition = QFUNC(canWrapJoint);
+        medicRequired = QGVAR(JointCareLevel);
+        treatmentTime = QGVAR(wrapTime);
+        items[] = {"kat_Elastic_Wrap"};
+    };
+    class Stabilize_Joint: BasicBandage {
+        displayName = CSTRING(Stabilize_Joint);
+        displayNameProgress = CSTRING(Stabilize_Joint_Progress);
+        icon = QPATHTOF(ui\Elastic_Wrap.paa);
+        category = "surgery";
+        callbackSuccess = QFUNC(stabilizeJoint);
+        condition = QFUNC(canStabilizeJoint);
+        medicRequired = QGVAR(JointCareLevel);
+        treatmentTime = QGVAR(stabilizeTime);
+        items[] = {"kat_Elastic_Wrap", "ACE_Splint"};
+    };
+    class Reduce_Joint: BasicBandage {
+        displayName = CSTRING(Reduce_Joint);
+        displayNameProgress = CSTRING(Reduce_Joint_Progress);
+        icon = QPATHTOF(ui\Elastic_Wrap.paa);
+        category = "surgery";
+        callbackSuccess = QFUNC(reduceJoint);
+        condition = QFUNC(canReduceJoint);
+        allowSelfTreatment = 0;
+        medicRequired = QGVAR(JointCareLevel);
+        treatmentTime = QGVAR(reduceTime);
+        items[] = {};
+    };
+    class Bind_Pelvis: BasicBandage {
+        displayName = CSTRING(Bind_Pelvis);
+        displayNameProgress = CSTRING(Bind_Pelvis_Progress);
+        icon = QPATHTOF(ui\Pelvic_Binder.paa);
+        category = "surgery";
+        callbackSuccess = QFUNC(bindPelvis);
+        condition = QFUNC(canBindPelvis);
+        allowSelfTreatment = 0;
+        medicRequired = QGVAR(JointCareLevel);
+        treatmentTime = QGVAR(bindTime);
+        items[] = {"kat_Pelvic_Binder"};
+    };
     class ApplyTourniquet: BasicBandage {
         allowedSelections[] = {"LeftArm", "RightArm", "LeftLeg", "RightLeg", "UpperLeftArm", "UpperRightArm", "UpperLeftLeg", "UpperRightLeg"};
     };
@@ -16,5 +159,47 @@ class ACE_Medical_Treatment_Actions {
     // - Diagnose -------------------------------------------------------------
     class Diagnose: BasicBandage {
         allowedSelections[] = {"Head", "Chest"};
-    };    
+    };
+    class CheckLimb: CheckPulse {
+        displayName = CSTRING(Check_Limb);
+        displayNameProgress = CSTRING(Check_Limb_Progress);
+        category = "examine";
+        condition = "true";
+        allowedSelections[] = {"All"};
+        allowSelfTreatment = 1;
+        medicRequired = 0;
+        treatmentTime = 2;
+        items[] = {};
+        callbackSuccess = QFUNC(checkLimb);
+    };
+    class CheckJoints: CheckPulse {
+        displayName = CSTRING(Check_Joints);
+        displayNameProgress = CSTRING(Check_Joints_Progress);
+        category = "examine";
+        condition = "true";
+        allowedSelections[] = {"LeftArm", "RightArm", "LeftLeg", "RightLeg", "UpperLeftArm", "UpperRightArm", "UpperLeftLeg", "UpperRightLeg"};
+        allowSelfTreatment = 1;
+        medicRequired = 0;
+        treatmentTime = 2;
+        items[] = {};
+        callbackSuccess = QFUNC(checkJoints);
+    };
+    class ApplyPressure: CPR {
+        displayName = CSTRING(ApplyPressure);
+        displayNameProgress = "";
+        icon = "";
+        category = "bandage";
+        treatmentTime = 0.01;
+        allowSelfTreatment = 1;
+        medicRequired = 0;
+        treatmentLocations = TREATMENT_LOCATIONS_ALL;
+        allowedSelections[] = {"All"};
+        callbackStart = "";
+        callbackProgress = "";
+        callbackFailure = "";
+        callbackSuccess = QFUNC(pressureStart);
+        condition = QFUNC(canBandage);
+        animationPatientUnconscious = "AinjPpneMstpSnonWrflDnon_rolltoback";
+        animationPatientUnconsciousExcludeOn[] = {"ainjppnemstpsnonwrfldnon"};
+    };
 };
---
+++ addons/hitpoints/functions/fnc_canWrapWound.sqf
@@ -0,0 +1,33 @@
+#include "..\script_component.hpp"
+/*
+ * Author: Cplhardcore
+ * Condition Check if you can wrap Wounds on a limb
+ *
+ * Arguments:
+ * 0: Medic <OBJECT>
+ * 1: Patient <OBJECT>
+ * 2: Body Part <STRING>
+ * 3: Treatment <STRING>
+
+ * Return Value:
+ * Bool
+ *
+ * Example:
+ * [player, cursorTarget, "Head", "FieldDressing"] call kat_hitpoints_fnc_canWrapWound
+ *
+ * Public: No
+ */
+
+params ["_medic", "_patient", "_bodyPart"];
+_bodyPart = toLowerANSI _bodyPart;
+
+private _bandagedWounds = GET_BANDAGED_WOUNDS(_patient);
+private _bandagedWoundsOnPart = _bandagedWounds get _bodyPart;
+if (isNil "_bandagedWoundsOnPart" || {_bandagedWoundsOnPart isEqualTo []}) exitWith {
+    false};
+
+private _includedTypes = ["Compressed_Gauze", "fourByfour_Gauze", "Burn_Dressing", "Hemostatic_Gauze"];
+
+(_bandagedWoundsOnPart findIf {
+    (_x param [4, ""]) in _includedTypes
+}) != -1
